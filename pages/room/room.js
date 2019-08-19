ad80d810c123d63653b55c43a1b10cdc3116aab0
xuchenmiao <1760000559@163.com>
2019-08-19 14:39:52 +0800

    找回代码

--- pages/room/room.js
+++ pages/room/room.js
@@ -1,11 +1,11 @@
 let user = require('../../data/user-list.js');
 let cardList = require('../../data/card-list.js');
-// pages/index/index.js
+// pages/index/index.js 
 const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
+  /** 
+   * 页面的初始数据 
    */
   data: {
     userInfo: {},
@@ -15,8 +15,8 @@ Page({
     isShowOpenBtn: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
+  /** 
+   * 生命周期函数--监听页面加载 
    */
   onLoad: function (options) {
     this.setData({
@@ -29,8 +29,8 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 
+      // 所以此处加入 callback 以防止这种情况 
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -38,7 +38,7 @@ Page({
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
+      // 在没有 open-type=getUserInfo 版本的兼容处理 
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
@@ -51,48 +51,48 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
+  /** 
+   * 生命周期函数--监听页面初次渲染完成 
    */
   onReady: function () { },
 
-  /**
-   * 生命周期函数--监听页面显示
+  /** 
+   * 生命周期函数--监听页面显示 
    */
   onShow: function () { },
 
-  /**
-   * 生命周期函数--监听页面隐藏
+  /** 
+   * 生命周期函数--监听页面隐藏 
    */
   onHide: function () { },
 
-  /**
-   * 生命周期函数--监听页面卸载
+  /** 
+   * 生命周期函数--监听页面卸载 
    */
   onUnload: function () { },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
+  /** 
+   * 页面相关事件处理函数--监听用户下拉动作 
    */
   onPullDownRefresh: function () { },
 
-  /**
-   * 页面上拉触底事件的处理函数
+  /** 
+   * 页面上拉触底事件的处理函数 
    */
   onReachBottom: function () { },
 
-  /**
-   * 用户点击右上角分享
+  /** 
+   * 用户点击右上角分享 
    */
   onShareAppMessage: function () { },
-  /**handleUserItemTap
-   * 点击用户位置
+  /**handleUserItemTap 
+   * 点击用户位置 
    */
   handleUserItemTap: function (item) {
     console.log(item, 'item');
   },
-  /**handleUserItemTap
-   * 点击用户位置
+  /**handleUserItemTap 
+   * 点击用户位置 
    */
   getUserInfo: function (e) {
     console.log(e)
@@ -102,8 +102,8 @@ Page({
       hasUserInfo: true
     })
   },
-  /**handleUserItemTap
-   * 点击准备按钮
+  /**handleUserItemTap 
+   * 点击准备按钮 
    */
   handleReadyBtnClick() {
     let animation = wx.createAnimation({
@@ -136,8 +136,8 @@ Page({
       isShowOpenBtn: true
     })
   },
-  /**handleUserItemTap
-   * 点击开牌
+  /**handleUserItemTap 
+   * 点击开牌 
    */
   handleOpenCard() {
     let cardBackAnimation = wx.createAnimation({
--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,65 +1,72 @@
-// pages/setting/setting.js
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-19 14:36:59
+ * @LastEditTime: 2019-08-19 14:37:48
+ * @LastEditors: Please set LastEditors
+ */
+// pages/setting/setting.js 
 var app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
+  /** 
+   * 页面的初始数据 
    */
   data: {
     userCount: app.data.userCount
   },
 
-  /**
-   * 生命周期函数--监听页面加载
+  /** 
+   * 生命周期函数--监听页面加载 
    */
   onLoad: function (options) {
-    
+
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
+  /** 
+   * 生命周期函数--监听页面初次渲染完成 
    */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
+  /** 
+   * 生命周期函数--监听页面显示 
    */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
+  /** 
+   * 生命周期函数--监听页面隐藏 
    */
   onHide: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
+  /** 
+   * 生命周期函数--监听页面卸载 
    */
   onUnload: function () {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
+  /** 
+   * 页面相关事件处理函数--监听用户下拉动作 
    */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
+  /** 
+   * 页面上拉触底事件的处理函数 
    */
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
+  /** 
+   * 用户点击右上角分享 
    */
   onShareAppMessage: function () {
 
